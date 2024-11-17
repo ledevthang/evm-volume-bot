6ff8fd3a5258c7bf151e74987a39b58893e62518
goni098 <mtheanh2910@gmail.com>
2024-11-17 10:43:49 +0700

    subaccount generate

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Web3 from "web3"
-import { Program } from "./program.js"
+import { type Config, Program } from "./program.js"
+import { privateKeyToAccount } from "web3-eth-accounts"
+import { ERC20 } from "./ecc20.abi.js"
 
 async function main() {
 	const rpcUrl = process.env.RPC
@@ -14,12 +16,23 @@ async function main() {
 
 	const web3 = new Web3(rpcUrl)
 
-	web3.eth.accounts.wallet.add(mainPk)
-
-	const program = new Program(web3, web3.eth.wallet?.get(0)!, {
+	const config: Config = {
 		chainId: 43114,
-		tokenAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7"
-	})
+		initFee: 0.02,
+		initToken: 0.02,
+		tokenAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+		subAccountConcurrency: 1,
+		subAccountTradingMax: 6
+	}
+
+	const erc20contract = new web3.eth.Contract(ERC20, config.tokenAddress)
+
+	const program = new Program(
+		web3,
+		privateKeyToAccount(mainPk),
+		config,
+		erc20contract
+	)
 
 	await program.run()
 }
--- src/program.ts
+++ src/program.ts
@@ -1,41 +1,88 @@
-import type { Address, Web3 } from "web3"
+import type { Address, Contract, Web3 } from "web3"
 import type { Web3Account } from "web3-eth-accounts"
 import {
 	type SwapParams,
 	generateApprove,
 	generateSwapCallData
 } from "./services.js"
+import fs from "node:fs/promises"
+import type { ERC20 } from "./ecc20.abi.js"
 
 const NATIVE = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+export type Config = {
+	chainId: number
+	tokenAddress: Address
+	initFee: number
+	initToken: number
+	subAccountConcurrency: number
+	subAccountTradingMax: number
+}
 
 type SubAccount = {
 	account: Web3Account
 	tradingTimes: number
 }
 
-type Config = {
-	chainId: number
-	tokenAddress: Address
-}
-
 export class Program {
 	constructor(
 		private web3: Web3,
 		private mainAccount: Web3Account,
-		private config: Config
-	) {}
-
-	public async run() {
-		// for (let i = 1; i < 10; i++)
-		await this.trade({
-			account: this.mainAccount,
-			tradingTimes: 1
-		})
+		private config: Config,
+		private erc20Contract: Contract<typeof ERC20>
+	) {
+		this.web3.eth.accounts.wallet.add(this.mainAccount)
 	}
 
-	private async trade(subAccount: SubAccount) {
-		// await this.approve(subAccount)
+	public async run() {
+		let subAccounts = await this.generateAccount(
+			this.config.subAccountConcurrency
+		)
+
+		await this.initTokensAndFee(subAccounts)
+
+		for (;;) {
+			const executingAccounts = subAccounts.map(({ account }) => ({
+				address: account.address,
+				privateKey: account.privateKey
+			}))
+
+			await fs.writeFile(
+				"executing-wallets.txt",
+				`${JSON.stringify(executingAccounts, null, 1)}\n`
+			)
+
+			subAccounts = await Promise.all(
+				subAccounts.map(subAccount => this.trade(subAccount))
+			)
+
+			await sleep(10e3)
+		}
+	}
+
+	private async trade(subAccount: SubAccount): Promise<SubAccount> {
+		if (subAccount.tradingTimes === this.config.subAccountTradingMax) {
+			const [newSubAccount] = await this.generateAccount(1)
+
+			await this.transferTokensAndFee(
+				subAccount.account.address,
+				newSubAccount.account.address,
+				1,
+				1
+			)
+			this.web3.eth.accounts.wallet.remove(subAccount.account.address)
+
+			return newSubAccount
+		}
+
+		if (subAccount.tradingTimes === 0) {
+			await this.approve(subAccount)
+		}
+
 		await this.swap(subAccount)
+
+		subAccount.tradingTimes++
+
+		return this.trade(subAccount)
 	}
 
 	private async approve(subAccount: SubAccount) {
@@ -57,28 +104,23 @@ export class Program {
 
 		const { rawTransaction } = await this.web3.eth.accounts.signTransaction(
 			data,
-			process.env.PRIVATE_KEY as string
+			subAccount.account.privateKey
 		)
-
-		console.log(rawTransaction)
 
 		const tx = await this.web3.eth.sendSignedTransaction(rawTransaction)
 
 		if (tx.status) {
-			console.log("approve success!")
+			console.log(`approve success from account ${subAccount.account.address}`)
 		}
 	}
 
 	private async swap(subAccount: SubAccount) {
-		const src =
-			subAccount.tradingTimes % 2 === 0 ? NATIVE : this.config.tokenAddress
-
-		const dst = src === NATIVE ? this.config.tokenAddress : NATIVE
+		const [src, dst] = this.getSrcAndDst(subAccount)
 
 		const swapParams: SwapParams = {
 			amount: this.web3.utils.toWei("0.001", "ether"),
-			dst,
 			src,
+			dst,
 			from: subAccount.account.address,
 			slippage: 1
 		}
@@ -88,7 +130,79 @@ export class Program {
 		const result = await this.web3.eth.sendTransaction(tx)
 
 		if (result.status) {
-			console.log("swap success!")
+			console.log(`swap success from account ${subAccount.account.address}`)
 		}
 	}
+
+	private async initTokensAndFee(subAccounts: SubAccount[]) {
+		for (const subAccount of subAccounts) {
+			await this.transferTokensAndFee(
+				this.mainAccount.address,
+				subAccount.account.address,
+				this.config.initToken,
+				this.config.initFee
+			)
+		}
+
+		console.log("initialized tokens and fee")
+	}
+
+	private async transferTokensAndFee(
+		from: Address,
+		to: Address,
+		tokenAmount: number,
+		feeAmount: number
+	) {
+		const tx = await this.web3.eth.accounts.signTransaction(
+			{
+				from,
+				to,
+				value: this.web3.utils.toWei(feeAmount, "ether")
+			},
+			this.mainAccount.privateKey
+		)
+
+		await this.web3.eth.sendTransaction(tx)
+
+		console.log(`transfered fee from ${from} to ${to} >> amount ${feeAmount}`)
+
+		await this.erc20Contract.methods
+			.transfer(to, this.web3.utils.toWei(this.config.initToken, "ether"))
+			.send({
+				from
+			})
+
+		console.log(`transfered tokens from ${from} to ${to} ${tokenAmount}`)
+	}
+
+	private async generateAccount(quantity: number): Promise<SubAccount[]> {
+		return Promise.all(
+			new Array(quantity).fill(1).map(async () => {
+				const account = this.web3.eth.accounts.create()
+				const data = JSON.stringify({
+					address: account.address,
+					privateKey: account.privateKey
+				})
+
+				this.web3.eth.accounts.wallet.add(account)
+
+				await fs.appendFile("wallets.txt", `${data}\n`)
+
+				return {
+					account,
+					tradingTimes: 0
+				}
+			})
+		)
+	}
+
+	private getSrcAndDst(subAccount: SubAccount) {
+		return subAccount.tradingTimes % 2 === 0
+			? [NATIVE, this.config.tokenAddress]
+			: [this.config.tokenAddress, NATIVE]
+	}
+}
+
+function sleep(duration: number) {
+	return new Promise(res => setTimeout(res, duration))
 }
